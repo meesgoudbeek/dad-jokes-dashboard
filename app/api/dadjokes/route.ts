9e1ea13c58237de9e3c302ae632f58b5bb4be715
Mees Goudbeek <meesgoudbeek@protonmail.com>
2024-12-11 16:10:32 +0100

    initial project setup and api fetching

---
+++ app/api/dadjokes/route.ts
@@ -0,0 +1,23 @@
+// app/api/dadjokes/route.ts
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const res = await fetch('https://api.api-ninjas.com/v1/dadjokes', {
+      method: 'GET',
+      headers: {
+        'X-Api-Key': 'w5dlActl9mbT1wxJOyjBuw==9ptWMjRhip5hyO4W',
+      },
+    });
+
+    if (!res.ok) {
+      return NextResponse.json({ error: 'Failed to fetch jokes' }, { status: 500 });
+    }
+
+    const jokes = await res.json();
+    return NextResponse.json(jokes);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return NextResponse.json({ error: 'Failed to fetch jokes' }, { status: 500 });
+  }
+}
